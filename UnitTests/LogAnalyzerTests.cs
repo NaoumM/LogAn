809a4eaa8d682be40884114d0b509447d7c95080
NaoumM <michail.naoum@gmail.com>
2017-04-17 23:44:13 +0100

    First 2 chapters of nunit

---
+++ UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using LogAn;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+
+        private LogAnalyzer _analyzer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _analyzer = new LogAnalyzer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+
+        [TestCase("badName.foo", false)]
+        [TestCase("goodName.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string filename, bool expected)
+        {
+            LogAnalyzer la = MakeAnalyzer();
+
+            la.IsValidLogFileName(filename);
+
+            Assert.AreEqual(expected, la.WasLastFileNameValid);
+        }
+
+        [Test]
+        [Ignore("Failing")]
+        [Category("Failed Test")]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer la = MakeAnalyzer();
+
+            var ex = Assert.Catch<Exception>(() => la.IsValidLogFileName(" "));
+
+            StringAssert.Contains("filename has to be provided", ex.Message);
+        }
+
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            bool result = _analyzer.IsValidLogFileName("fileWithBadExtension.foo");
+
+            Assert.False(result);
+        }
+
+        [TestCase("fileWithGoodExtension.slf")]
+        [TestCase("fileWithGoodExtension.SLF")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            bool result = _analyzer.IsValidLogFileName(file);
+
+            Assert.True(result);
+        }
+
+    }
+}
